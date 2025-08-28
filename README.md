8abffdc2e0283accec1b22ade368d6533f2cafbe
JagadishReddyK7 <kollu.jagadishreddy@gmail.com>
2025-08-28 18:19:37 +0530

    Implemented spell checker using trie

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Spell Checker, Autocomplete, Autocorrect using Trie
+
+This program implements an efficient spell checker using a **Trie** datastructure to support three different functionalities.
+
+---
+
+### Approach
+
+The solution is built around a Trie that stores the dictionary of `n` wordscite: 70, 71. Each query is then processed based on its type.
+
+1.  **Spell Check (Type 1)**: This function checks if an input string is present in the dictionarycite: 66, 72. It's implemented with a standard traversal of the Trie. If the traversal completes and the final node is marked as the end of a word, it returns '1', otherwise '0'cite: 80.
+
+2.  **Autocomplete (Type 2)**: This function finds all words in the dictionary that begin with the given input stringcite: 67, 73. The code first traverses the Trie to the node representing the prefix. From that node, it performs a **Depth First Search (DFS)** to find all valid words in the sub-trie and prints them in lexicographical ordercite: 82.
+
+3.  **Autocorrect (Type 3)**: This function finds all words in the dictionary that are within a **Levenshtein distance of at most 3** from the given input stringcite: 68, 74. A recursive DFS explores the Trie while tracking the current edit distance. The search is pruned if the distance exceeds 3, making it efficient. The results are also printed in lexicographical ordercite: 82.
+
+**Compilation Command:**
+
+        $ g++ trie.cpp -o trie
---
+++ trie.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Query {
+    int type;
+    string s;
+};
+
+struct TrieNode {
+    TrieNode* next[26];
+    bool endOfWord;
+    bool visited;
+    TrieNode() {
+        for(int i=0;i<26;i++) {
+            next[i]=nullptr;
+        }
+        endOfWord=false;
+        visited=false;
+    }
+};
+
+class Trie {
+    private:
+        TrieNode* root;
+    public:
+        Trie() {
+            root = new TrieNode();
+        }
+        TrieNode* get_root() {
+            return root;
+        }
+        void insert(string word) {
+            TrieNode* node=root;
+            for(char ch: word) {
+                int index=ch-'a';
+                if(!node->next[index]) {
+                    node->next[index]=new TrieNode();
+                }
+                node=node->next[index];
+            }
+            node->endOfWord=true;
+        }
+        bool search(string word) {
+            TrieNode* node=root;
+            for(char ch: word) {
+                int index=ch-'a';
+                if(!node->next[index]) {
+                    return false;
+                }
+                node=node->next[index];
+            }
+            return node->endOfWord;
+        }
+        TrieNode* nodeOfPrefix(string prefix) {
+            TrieNode* node=root;
+            for(char ch:prefix) {
+                int index=ch-'a';
+                if(!node->next[index]) {
+                    return nullptr;
+                }
+                node=node->next[index];
+            }
+            return node;
+        }
+        void searchFromHere(TrieNode* node, string ssf, vector<string>& matchings) {
+            if(node->endOfWord) {
+                matchings.push_back(ssf);
+            }
+            for(int i=0;i<26;i++) {
+                if(node->next[i]) {
+                    char ch=(char)i+'a';
+                    searchFromHere(node->next[i], ssf+ch, matchings);
+                }
+            }
+        }
+        void recurse(TrieNode* node, int i, int k, string s, string ssf, vector<string>& res) {
+            if(k>3) return;
+            int len=s.size();
+            if(node->endOfWord && i==len && k<=3) {
+                if(!node->visited) {
+                    res.push_back(ssf);
+                    node->visited=true;
+                }
+            } 
+            if(node->endOfWord) {
+                k+=len-i;
+                if(k<=3) {
+                    if(!node->visited) {
+                        res.push_back(ssf);
+                        node->visited=true;
+                    }
+                }
+                k-=len-i;
+            }
+            if(i==len) {
+                for(int j=0;j<26;j++) {
+                    char ch='a'+j;
+                    if(node->next[j]!=NULL) {
+                        recurse(node->next[j], i, k+1, s, ssf+ch, res);
+                    }
+                }
+            }
+            for(int j=0;j<26;j++) {
+                if(node->next[j]!=NULL) {
+                    char ch='a'+j;
+                    if(s[i]==ch) {
+                        recurse(node->next[j], i+1, k, s, ssf+ch, res);
+                    }
+                    recurse(node->next[j], i+1, k+1, s, ssf+ch, res);
+                    recurse(node, i+1, k+1, s, ssf, res);
+                    recurse(node->next[j], i, k+1, s, ssf+ch, res);
+                }
+            }
+        }
+        void devisit(TrieNode* node) {
+            if(node->endOfWord) node->visited=false;
+            for(int i=0;i<26;i++) {
+                if(node->next[i]!=NULL) {
+                    devisit(node->next[i]);
+                }
+            }
+        }
+};
+
+int main() {
+    Trie trie;
+    int n, q;
+    cin >> n >> q;
+    Query queries[q];
+    string s;
+    for(int i=0;i<n;i++) {
+        cin >> s;
+        trie.insert(s);
+    }
+    for(int i=0;i<q;i++) {
+        cin >> queries[i].type >> queries[i].s;
+    }
+    for(int i=0;i<q;i++) {
+        if(queries[i].type==1) {
+            cout << trie.search(queries[i].s) << endl;
+        }
+        else if(queries[i].type==2) {
+            vector<string> v;
+            int num;
+            TrieNode* temp=trie.nodeOfPrefix(queries[i].s);
+            if(temp!=NULL) {
+                trie.searchFromHere(temp, queries[i].s, v);
+            }
+            num=v.size();
+            cout<<num<<endl;
+            for(int i=0;i<num;i++) {
+                cout << v[i]<<endl;
+            }
+        }
+        else if(queries[i].type==3) {
+            TrieNode* temp_node=trie.get_root();
+            vector<string> auto_correct;
+            trie.recurse(temp_node, 0, 0, queries[i].s, "", auto_correct);
+            int len=auto_correct.size();
+            cout<<len<<endl;
+            for(int i=0;i<len;i++) {
+                cout<<auto_correct[i]<<endl;
+            }
+            TrieNode* temp_node1=trie.get_root();
+            trie.devisit(temp_node1);
+        }
+        else {
+            cout<<"Invalid flag"<<endl;
+        }
+    }
+    // cout << trie.search(s) << endl;
+    // vector<string> v;
+    // int n;
+    // TrieNode* temp=trie.nodeOfPrefix(s);
+    // if(temp!=NULL) {
+    //     trie.searchFromHere(temp, s, v);
+    // }
+    // n=v.size();
+    // cout<<n<<endl;
+    // for(int i=0;i<n;i++) {
+    //     cout << v[i]<<endl;
+    // }
+
+    // TrieNode* temp_node=trie.get_root();
+    // vector<string> auto_correct;
+    // trie.recurse(temp_node, 0, 0, s, "", auto_correct);
+    // int len=auto_correct.size();
+    // cout<<len<<endl;
+    // for(int i=0;i<len;i++) {
+    //     cout<<auto_correct[i]<<endl;
+    // }
+    // TrieNode* temp_node1=trie.get_root();
+    // trie.devisit(temp_node1);
+    return 0;
+}
